9994f0e321d2b4256e759df03cd60e1823c827fc
Hy-u-3a4eM <za4emsu@gmail.com>
2024-01-28 21:48:37 +0300

    Update 20240128134535_init.up.sql

--- migrations/20240128134535_init.up.sql
+++ migrations/20240128134535_init.up.sql
@@ -12,10 +12,10 @@ CREATE TABLE
         password VARCHAR(100) NOT NULL,
         role VARCHAR(50) NOT NULL DEFAULT 'user',
         created_at TIMESTAMP
-        WITH
+            WITH
             TIME ZONE DEFAULT NOW(),
         updated_at TIMESTAMP
-        WITH
+            WITH
             TIME ZONE DEFAULT NOW(),
     );
 
